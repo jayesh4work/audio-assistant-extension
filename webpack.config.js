82c1b689045df6184e3bd044c1629b931a551a30
JayeshWork <jayesh4work@gmail.com>
2026-01-14 23:47:28 +0530

    minor issues fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,52 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    popup: './src/popup/popup.tsx',
-    background: './src/background/worker.ts',
-    content: './src/content/script.ts',
+    popup: "./src/popup/popup.tsx",
+    background: "./src/background/worker.ts",
+    content: "./src/content/script.ts",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/popup.html',
-      filename: 'popup.html',
-      chunks: ['popup'],
+      template: "./public/popup.html",
+      filename: "popup.html",
+      chunks: ["popup"],
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'public/manifest.json', to: 'manifest.json' },
-        { from: 'public/icons', to: 'icons', noErrorOnMissing: true },
+        { from: "public/manifest.json", to: "manifest.json" },
+        { from: "public/icons", to: "icons", noErrorOnMissing: true },
       ],
     }),
   ],
-  devtool: 'source-map',
+  devtool: "source-map",
 };
